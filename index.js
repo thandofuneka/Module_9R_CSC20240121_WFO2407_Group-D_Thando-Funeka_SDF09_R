f5355bbd97a6b7e3231fedabddcb7cb9e724c9b0
Thando <thandofuneka1@.com>
2024-07-31 16:42:28 +0200

    Added CSS file

--- index.js
+++ index.js
@@ -3,9 +3,23 @@
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
 
-let firstCard = 6
-let secondCard = 9
-
+let firstCard = 10
+let secondCard = 10
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = " "
 
-console.log(sum)
+if (sum <= 20) {
+    message = "Do you want to draw a new card?"
+}else if(sum === 21) {
+    message = "WOHOO! You've got Blackjack!"
+    hasBlackJack = true
+}else {
+    message = "You're out of the game!"
+    isAlive = false
+}
+
+console.log(message)
+
+
